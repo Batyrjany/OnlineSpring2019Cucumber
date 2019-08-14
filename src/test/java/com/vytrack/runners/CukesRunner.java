9f1ce5ef3cbfa165a6c0f0bd4f6e7049b94c9825
Batyrjany <b.janyew@gmail.com>
2019-08-13 19:29:35 -0700

    08.13.2019

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -1,22 +1,22 @@
 package com.vytrack.runners;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {
-            "src/test/resources/features" //to specify where are the features
+        plugin = {
+                "html:target/default-cucumber-reports"
+        },
+        tags = {"@vehicles"},
+        features = {"src/test/resources/features" //to specify where are the features
         },
         //feature contains scenarios
         //every scenario is like a test
         //where is the implementation for features
         glue = {"com/vytrack/step_definitions"},
-        //dry tun - to generate step definitions automatically
+        //dry run - to generate step definitions automatically
         //you will see them in the console output
-        dryRun = true
-
+        dryRun = false
 )
 public class CukesRunner {
 }
