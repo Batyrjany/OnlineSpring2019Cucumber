3a3f20a40ff5b07d16806770430cd9a1784270c9
Batyrjany <b.janyew@gmail.com>
2020-04-21 10:43:09 -0500

    04/21/

--- src/test/java/com/vytrack/runners/CukesRunner3.java
+++ src/test/java/com/vytrack/runners/CukesRunner3.java
@@ -17,10 +17,10 @@ import org.junit.runner.RunWith;
         //feature contains scenarios
         //every scenario is like a test
         //where is the implementation for features
-        glue = {"com/vytrack/step_definitions"},
+        glue = {"com/vytrack/step_definitions"}
         //dry run - to generate step definitions automatically
         //you will see them in the console output
-        dryRun = false
+//       , dryRun = true
 
 )
 public class CukesRunner3 {
